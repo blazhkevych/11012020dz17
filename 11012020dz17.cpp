f0e73f2adb443e0a30b5a94f4bbe668d5e3926bc
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:23:50 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz17.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры число.Вывести на 	экран это число в квадрате, кубе и в 4й 	степени.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int a, b, c, d;
+	cout << "Введите число : " << endl;
+	cin >> a; //введенное число
+	d = a * a; //квадрат числа
+	b = a * a * a; //куб
+	c = a * a * a * a; //4 степ
+	cout << "Квадрат: " << d << ", куб: " << b << ", 4тая степень: " << c << "." << endl;
+	return 0;
+}
